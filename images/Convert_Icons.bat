2877471a99556aa70d272766307aa3aad083e7a2
Michael 'Tex' Hex <texhex@users.noreply.github.com>
2015-01-08 19:43:23 +0100

    Added images

--- images/Convert_Icons.bat
+++ images/Convert_Icons.bat
@@ -7,8 +7,8 @@ REM  http://www.axialis.com/download/iw.html
 
 REM Convert using http://www.imagemagick.org/
 
-SET EXE_CONVERT=C:\Program Files (x86)\_Tools\ImageMagick-6.9.0-2\convert.exe
+SET EXE_CONVERT=C:\_Tools\ImageMagick-6.9.0-2\convert.exe
 REM -colors 256 
-"%EXE_CONVERT%" TestUtil_Icon_16x16.ico TestUtil_Icon_32x32.ico TestUtil_Icon_48x48.ico TestUtil_Icon.ico
+"%EXE_CONVERT%" Xteq5_Icon_16x16.ico Xteq5_Icon_32x32.ico Xteq5_Icon_48x48.ico Xteq5_Icon.ico
 
 pause
