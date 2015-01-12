b6a4f6dc03ca2b6bd38119ea25291ba0b5c46be5
Michael 'Tex' Hex <texhex@users.noreply.github.com>
2015-01-12 20:55:33 +0100

    Added BaseException

---
+++ src/Yamua/BaseException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yamua
+{
+    [Serializable]
+    //A base class for custom exceptions. 
+    public abstract class BaseException : Exception
+    {
+        public BaseException()
+        { }
+
+        public BaseException(string Message)
+            : base(Message) { }
+
+        public BaseException(string FormatString, params object[] Args)
+            : base(string.Format(FormatString, Args)) { }
+
+        public BaseException(string Message, Exception InnerException)
+            : base(Message, InnerException) { }
+
+        public BaseException(string FormatString, Exception InnerException, params object[] Args)
+            : base(string.Format(FormatString, Args), InnerException) { }
+
+    }
+}
